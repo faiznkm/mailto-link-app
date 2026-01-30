ee0b96881fb24aa510dcc679662ca162f6f1fc1c
Faiz TA <faiznkm@gmail.com>
2026-01-30 15:32:57 +0530

    Goe Location Update

--- src/app/api/log-request/route.ts
+++ src/app/api/log-request/route.ts
@@ -66,7 +66,9 @@ export async function POST(req: Request) {
       } catch (geoError) {
         console.log("Geo lookup failed:", geoError);
       }
+
     }
+    console.log("Saving Location:", { city, region, country });
 
     // ✅ Insert into Supabase
     const { error } = await supabaseServer.from("email_requests").insert([
